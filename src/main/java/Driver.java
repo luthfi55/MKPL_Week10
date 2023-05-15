4252de055c90e98e34917213905dac8d91975fdf
luthfi55 <luthfi12354@gmail.com>
2023-05-15 14:27:10 +0700

    contoh testing gagal

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -6,7 +6,7 @@ public class Driver {
 		
 		Counter counter = new Counter();
 		
-		System.out.println("Count:" + counter.getCountss());
+		System.out.println("Count:" + counter.getCounts());
 		
 		counter.increment();
 		
