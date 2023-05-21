84f87fdb675089edda9a623249843369a41366c8
luthfi55 <luthfi12354@gmail.com>
2023-05-21 09:45:26 +0700

    Contoh sonarcloud gagal

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -15,6 +15,10 @@ public class Driver {
 		counter.decrement();
 		
 		System.out.println("Counter:" + counter.getCount());
+
+		counter.decrement();
+		
+		System.out.println("Counter:" + counter.getCount());
 		
 	}
 
